57e21c1bdf35082ff67a3e3696a88ef478a3a5c1
rlawlgns077 <rlawlgns077@gmail.com>
2019-04-12 16:46:40 +0900

    firebase 기본 연동

---
+++ android/DocsCare/app/src/main/java/com/ddd/docscare/sns/DcFirebaseMessagingService.kt
@@ -0,0 +1,78 @@
+package com.ddd.docscare.sns
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import com.ddd.docscare.MainActivity
+import com.ddd.docscare.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class DcFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String?) {
+        super.onNewToken(token)
+
+        sendRegistration(token)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+    }
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
+    }
+
+    private fun sendRegistration(token: String?) {
+        if(token == null) return
+
+        // 토큰을 푸시 서버에 저장하기 위해 SharedPreference 에 저장한다
+        // 사용자구분을 해야하기때문에 SNS ID 값과 같이 보내야한다.
+    }
+
+    private fun sendNotification(body: String?) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("Notification", body)
+        }
+
+        val notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE)
+            channel.description = DESCRIPTION
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.setShowBadge(false)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder = NotificationCompat.Builder(this,"Notification")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Push Notification FCM")
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(notificationSound)
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+
+    companion object {
+        const val CHANNEL_ID = "Docs Care Notification"
+        const val CHANNEL_NAME = "Docs Care Channel"
+        const val DESCRIPTION = "This is Docs Care channel"
+        const val IMPORTANCE = NotificationManager.IMPORTANCE_HIGH
+    }
+}

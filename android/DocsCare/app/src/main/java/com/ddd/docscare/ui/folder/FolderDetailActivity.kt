b80e4348b5b7e481a57d2114e2c8e23b38ce5b0b
kim <rlawlgns077@gmail.com>
2019-06-06 16:03:26 +0900

    1. 폴더안에 아이템 리스트 UI 구현 2. UI 미리보기

---
+++ android/DocsCare/app/src/main/java/com/ddd/docscare/ui/folder/FolderDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.ddd.docscare.ui.folder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ddd.docscare.R
+
+class FolderDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_folder_detail)
+    }
+}
